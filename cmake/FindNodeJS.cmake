325975d6f83afe2af9bfe71e31024982c94437f5
Kesh Ikuma <hokiedsp@users.noreply.github.com>
2018-03-13 09:30:09 -0500

    cont' on FindNodeJS.cmake

--- cmake/FindNodeJS.cmake
+++ cmake/FindNodeJS.cmake
@@ -23,63 +23,221 @@
 # In Windows, it looks for NLOPT_DIR environment variable if defined
 
 
+# pkg_check_modules(PC_NodeJS QUIET NodeJS)
 
+find_program(NodeJS_EXECUTABLE node)
+get_filename_component(NodeJS_BIN_DIR ${NodeJS_EXECUTABLE} DIRECTORY)
 
+execute_process(COMMAND "${NodeJS_EXECUTABLE}" --version OUTPUT_VARIABLE NodeJS_Ver)
+string(STRIP ${NodeJS_Ver} NodeJS_Ver)
+string(SUBSTRING ${NodeJS_Ver} 1 -1 NodeJS_Ver)
+message("NodeJS_Ver: ${NodeJS_Ver}")
 
-https://nodejs.org/dist/v9.4.0/win-x64/node.lib   
-https://nodejs.org/dist/v9.4.0/win-x86/node.lib   
-https://nodejs.org/dist/v9.4.0/node-v9.4.0-headers.tar.gz
-
-CMAKE_SHARED_LINKER_FLAGS:STRING=/machine:x64
-
-include(FindPackageHandleStandardArgs)
-
-# ###################################
-# Exploring the possible NLOPT_ROOT
+# Get native target architecture
+include(CheckSymbolExists)
 if(WIN32)
-    if(NOT DEFINED NLOPT_ROOT)
-        #look in the system environmental variable named "NLOPT_DIR"
-        set(NLOPT_ROOT $ENV{NLOPT_DIR} CACHE PATH "NLOPT installation root path")
+  check_symbol_exists("_M_AMD64" "" RTC_ARCH_X64)
+  if(NOT RTC_ARCH_X64)
+    check_symbol_exists("_M_IX86" "" RTC_ARCH_X86)
+    if (NOT RTC_ARCH_X86)
+      check_symbol_exists("_M_ARM" "" RTC_ARCH_ARM)
     endif()
-    if(DEFINED NLOPT_ROOT AND NOT EXISTS ${NLOPT_ROOT})
-        # if NLOPT_ROOT specified but erroneous
-        message(WARNING "[NLOPT] the specified path for NLOPT_ROOT does not exist (${NLOPT_ROOT})")
+  endif(NOT RTC_ARCH_X64)
+  # add check for arm here
+  # see http://msdn.microsoft.com/en-us/library/b0084kay.aspx
+else(WIN32)
+  check_symbol_exists("__x86_64__" "" RTC_ARCH_X64)
+  if(NOT RTC_ARCH_X64)
+    check_symbol_exists("__i386__" "" RTC_ARCH_X86)
+    if (NOT RTC_ARCH_X86)
+      check_symbol_exists("__arm__" "" RTC_ARCH_ARM)
     endif()
+  endif(NOT RTC_ARCH_X64)
+endif(WIN32)
+
+if(RTC_ARCH_X64)
+  set(ARCH_STR x64)
+elseif(RTC_ARCH_X86)
+  set(ARCH_STR x86)
+elseif(RTC_ARCH_ARM)
+  set(ARCH_STR ARM)
 else()
-  # Linux & Mac should not need one
-  set(NLOPT_ROOT "" CACHE PATH "NLOPT installation root path")
+  message(FATAL_ERROR "Unknown architecture")
 endif()
 
-include (GNUInstallDirs) # defines CMAKE_INSTALL_LIBDIR & CMAKE_INSTALL_INCLUDEDIR
+# set command options to run npm
+if (WIN32)
+  set(NPM "cmd")
+  list(APPEND NPM_ARGS "/c" npm)
+else(WIN32)
+  set(NPM "npm")
+  list(LENGTH APPEND)
+endif(WIN32)
 
-# Find header and lib directories
-find_path(NLOPT_INCLUDE_DIR nlopt.h
-    PATHS
-    ${NLOPT_ROOT}
-    ${CMAKE_INSTALL_FULL_INCLUDEDIR}
-    ${CMAKE_INSTALL_FULL_OLDINCLUDEDIR}
-    "$ENV{ProgramFiles}/NLopt"
-    "$ENV{USERPROFILE}/AppData/Local"
-    "$ENV{USERPROFILE}/AppData/Local/Programs"
-    "$ENV{USERPROFILE}/AppData/Local/include"
-    "$ENV{SystemDrive}"
-    PATH_SUFFIXES NLOPT
-    DOC "Location of NLopt header file"
-)
+# define the function to look for a node package (runs npm)
+function(FindNodePackage dir ver pkg)
+  # check the local node_modules first
+  execute_process(COMMAND ${NPM} ${NPM_ARGS} list ${pkg} OUTPUT_VARIABLE list_output)
+  string(STRIP ${list_output} list_output)
+  string(FIND ${list_output} "\n" pos REVERSE)
+  if (pos EQUAL -1)
+    message(FATAL_ERROR "expects 2 output lines from \"npm list pkg\"")
+  endif()
+  math(EXPR pos "${pos}+1")
+  string(SUBSTRING ${list_output} ${pos} -1 pkg_info)
+  string(FIND ${pkg_info} "@" at_pos)
+  if (at_pos EQUAL -1) # not found, try global
+    execute_process(COMMAND ${NPM} ${NPM_ARGS} list ${pkg} -g OUTPUT_VARIABLE list_output)
+    string(STRIP ${list_output} list_output)
+    string(FIND ${list_output} "\n" pos REVERSE)
+    if (pos EQUAL -1)
+      message(FATAL_ERROR "expects 2 output lines from \"npm list pkg -g\"")
+    endif()
+    math(EXPR pos "${pos}+1")
+    string(SUBSTRING ${list_output} ${pos} -1 pkg_info)
+    string(FIND ${pkg_info} "@" at_pos)
+  endif()
 
-find_library(NLOPT_LIBRARY
-    NAMES nlopt libnlopt
-    PATHS
-    ${NLOPT_ROOT}
-    ${CMAKE_INSTALL_FULL_LIBDIR}
-    "$ENV{ProgramFiles}/NLopt"
-    "$ENV{USERPROFILE}/AppData/Local"
-    "$ENV{USERPROFILE}/AppData/Local/Programs"
-    "$ENV{USERPROFILE}/AppData/Local/lib"
-    "$ENV{SystemDrive}"
-    DOC "Location of NLopt library"
-)
+  if (NOT (at_pos EQUAL -1))
+    # retrieve the version string
+    math(EXPR at_pos "${at_pos}+1")
+    string(SUBSTRING ${pkg_info} ${at_pos} -1 ver_)
+    set(${ver} ${ver_} PARENT_SCOPE)
+  
+    # first line contains the path
+    string(FIND ${list_output} " " pos)
+    math(EXPR pos "${pos}+1")
+    string(FIND ${list_output} "\n" len)
+    math(EXPR len ${len}-${pos})
+    string(SUBSTRING ${list_output} ${pos} ${len} dir_) # path to the parent of node_modules
+    file(TO_CMAKE_PATH ${dir_} dir_)
+    string (CONCAT dir_ ${dir_}   "/node_modules/" ${pkg})
+    set(${dir} ${dir_} PARENT_SCOPE)
+  endif()
+endfunction(FindNodePackage)
 
-find_package_handle_standard_args(
-  NLOPT
-  REQUIRED_VARS NLOPT_INCLUDE_DIR NLOPT_LIBRARY)
+# sort requested components into runtime & library
+foreach(component IN LISTS NodeJS_FIND_COMPONENTS)
+  if (component MATCHES .+_RUNTIME)
+    list(APPEND runtime_component ${component})
+  elseif (component MATCHES .+_LIBRARY)
+    message("Found LIBRARY: ${component}")
+    list(APPEND library_component ${component})
+  else()
+    message(FATAL_ERROR "Unknown component: ${component}")
+  endif()
+endforeach(component IN LISTS NodeJS_FIND_COMPONENTS)
+
+# only 1 RUNTIME component maybe requested
+if (DEFINED runtime_component)
+  list(LENGTH runtime_component num_runtime)
+  if (${num_runtime} GREATER 1)
+    message(FATAL_ERROR "Cannot set multiple RUNTIME components.")
+  endif()
+  list(GET runtime_component 0 runtime_component)
+else()
+  set(runtime_component "NODE_RUNTIME") # default runtime
+endif()
+message("Found RUNTIME: ${runtime_component}")
+
+# Create root directory to store runtime headers and .lib files
+set(NodeJS_RUNTIME_ROOT_DIR "${CMAKE_BINARY_DIR}" CACHE PATH "Root directory of where to load/save node runtime")
+if (NOT IS_DIRECTORY ${NodeJS_RUNTIME_ROOT_DIR})
+  message(FATAL_ERROR "NodeJS_RUNTIME_ROOT_DIR is not a valid directory (${NodeJS_RUNTIME_ROOT_DIR}).")
+endif()
+
+function (FindNodeJSRuntime root incl_suffix headerurl lib_suffix winliburl)
+  # check if the runtime header files have been downloaded already
+  find_path (NodeJS_RUNTIME_INCLUDE_DIR node.h
+             HINTS ${root}
+             PATH_SUFFIXES ${incl_suffix}
+             DOC "Include directory of Node runtime library"
+             NO_DEFAULT_PATH)
+
+  # if not available, download and unpack the header files
+  if (NOT EXISTS NodeJS_RUNTIME_INCLUDE_DIR)
+    get_filename_component(DL_DST ${headerurl} NAME)
+    set(DL_DST "${CMAKE_BINARY_DIR}/${EL_DST}")
+    file(DOWNLOAD ${headerurl}
+         ${DL_DST})
+    execute_process(COMMAND "cmake" -E tar xzf "${DL_DST}") # unpack root
+  endif()
+
+  # find the runtime library file
+  find_library (NodeJS_RUNTIME_LIBRARY node
+                HINTS ${root}
+                PATH_SUFFIXES ${lib_suffix}
+                DOC "Node runtime library")
+
+  # if not available additionally download .lib file for windows build
+  if (WIN32 AND NOT NodeJS_RUNTIME_LIBRARY)
+    set(DL_DST "${root}/lib/node.lib")
+    file(DOWNLOAD ${winliburl} ${DL_DST})
+    if (EXISTS ${DL_DST})
+      set(NodeJS_RUNTIME_LIBRARY "${DL_DST}" CACHE PATH "NodeJS runtime library" FORCE)
+    endif ()
+  endif ()
+endfunction(FindNodeJSRuntime)
+
+if (runtime_component STREQUAL NODE_RUNTIME)
+  # Create root directory in build directory (name=packed directory name)
+  set(Node_RUNTIME_ROOT_DIR "${NodeJS_RUNTIME_ROOT_DIR}/node-v${NodeJS_Ver}")
+  FindNodeJSRuntime(Node_RUNTIME_ROOT_DIR 
+                    "include/node" 
+                    "https://nodejs.org/dist/v${NodeJS_Ver}/node-v${NodeJS_Ver}-headers.tar.gz" 
+                    "lib"
+                    "https://nodejs.org/dist/v${NodeJS_Ver}/win-${ARCH_STR}/node.lib")
+elseif (runtime_component STREQUAL ELECTRON_RUNTIME)
+  # electron package must be installed
+  FindNodePackage(NodeJS_Electron_Dir NodeJS_Electron_Ver electron)
+  FindNodeJSRuntime(Node_RUNTIME_ROOT_DIR 
+                    "include/node" 
+                    "https://atom.io/download/atom-shell/v${NodeJS_Electron_Ver}/node-v${NodeJS_Electron_Ver}.tar.gz" 
+                    "lib"
+                    "https://nodejs.org/dist/v${NodeJS_Ver}/win-${ARCH_STR}/node.lib")
+  message("electron package@${NodeJS_Electron_Ver} found at ${NodeJS_Electron_Dir}")
+else()
+  message(FATAL_ERROR "${runtime_component} is not a valid or supported RUNTIME component.")
+endif (runtime_component STREQUAL NODE_RUNTIME)
+  
+# # https://atom.io/download/atom-shell/v1.8.2/node-v1.8.2.tar.gz
+# # https://atom.io/download/atom-shell/v1.8.2/x64/node.lib
+# # https://atom.io/download/atom-shell/v1.8.2/node.lib
+# endif (NodeJS_RUNTIME EQUAL 0)
+
+
+
+# # Check for NAN
+# if (NOT EXISTS NodeJS_NAN_FOUND)
+#   # check the local modules first
+#   FindNodePackage(NodeJS_NAN_Dir NodeJS_NAN_Ver nan)
+#   if (NodeJS_NAN_Ver)
+#     set(NodeJS_NAN_FOUND ON CACHE BOOL "ON if NodeJS NAN package is installed")
+#     set(NodeJS_NAN_INCLUDE_DIR "${NodeJS_NAN_Dir}" CACHE PATH "Include directory for NodeJS NAN package")
+#   else()
+#     set(NodeJS_NAN_FOUND OFF CACHE BOOL "ON if NodeJS NAN package is installed")
+#   endif()
+# endif()
+
+# include(FindPackageHandleStandardArgs)
+# find_package_handle_standard_args(NodeJS
+#   FOUND_VAR NodeJS_FOUND
+#   REQUIRED_VARS
+#     NodeJS_LIBRARY
+#     NodeJS_INCLUDE_DIR
+#   VERSION_VAR NodeJS_VERSION
+# )
+
+# if(NodeJS_FOUND)
+#   set(NodeJS_LIBRARIES ${NodeJS_LIBRARY})
+#   set(NodeJS_INCLUDE_DIRS ${NodeJS_INCLUDE_DIR})
+#   set(NodeJS_DEFINITIONS ${PC_NodeJS_CFLAGS_OTHER})
+# endif()
+
+# mark_as_advanced(
+#   NodeJS_INCLUDE_DIR
+#   NodeJS_LIBRARY
+# )
+
+# # compatibility variables
+# set(NodeJS_VERSION_STRING ${NodeJS_VERSION})
