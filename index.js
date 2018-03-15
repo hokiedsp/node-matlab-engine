9705bc041531104831a9b1d1b58a23def1a79ed4
Kesh Ikuma <hokiedsp@users.noreply.github.com>
2018-03-15 09:47:58 -0500

    dir reorg

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let lib = require("./lib");
+
+// let _ = require("lodash"); let ext = require("./ext"); let Bluebird =
+// require("bluebird"); let debug = require("debug")("af");
+
+let matlabEngine = null;
+
+let entry = module.exports = function () {
+  let ml = matlabEngine || (matlabEngine = require("bindings")("./build/ml_eng"));
+  // if (!af.__extended) {     ext(af);     af.__extended = true; }
+  return ml;
+};
