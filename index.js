87a9d9d02314eaab6fd76f52326620b32717c3f7
Takeshi Ikuma <tikuma@gmail.com>
2018-05-15 09:44:16 -0500

    fixed native binding

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 "use strict";
 
-let lib = require("./lib");
+module.exports = require("bindings")("src/addon.node");
+
+// let lib = require("./lib");
 
 // let _ = require("lodash"); let ext = require("./ext"); let Bluebird =
 // require("bluebird"); let debug = require("debug")("af");
 
-let matlabEngine = null;
+// let matlabEngine = null;
 
-let entry = module.exports = function () {
-  let ml = matlabEngine || (matlabEngine = require("bindings")("./build/src"));
-  // if (!af.__extended) {     ext(af);     af.__extended = true; }
-  return ml;
-};
+// let entry = module.exports = function () {
+//   let ml = matlabEngine || (matlabEngine = require("bindings")("./build/src/addon.node"));
+//   // if (!af.__extended) {     ext(af);     af.__extended = true; }
+//   return ml;
+// };
